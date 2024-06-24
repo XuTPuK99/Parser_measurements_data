e97db8584f931079c344eb926318845657b5e3f9
XuTPuK99 <ivan.tyurnev.99@mail.ru>
2024-06-24 16:03:24 +0800

    init commit

--- config_parser.py
+++ config_parser.py
@@ -1,6 +1,5 @@
-import re
 from datetime import datetime
-from typing import Optional, Union, List
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -14,7 +13,7 @@ class ConductivitySensor(Sensor):
     conductivity_B: Optional[float] = None
     conductivity_C: Optional[float] = None
     conductivity_D: Optional[float] = None
-    conductivity_PCOR: Optional[float] = None
+    conductivity_CPCOR: Optional[float] = None
     conductivity_cell_const: Optional[float] = None
     conductivity_series_r: Optional[float] = None
     conductivity_slope: Optional[float] = None
@@ -58,47 +57,91 @@ class SensorBuilder:
 
 class ConductivitySensorBuilder(SensorBuilder):
     def get(self, num):
-        return SensorBuilder(
-            conductivity_M = self.conf_data[num],
-            conductivity_A = self.conf_data[num + 1],
-            conductivity_B = self.conf_data[num + 2],
-            conductivity_C = self.conf_data[num + 3],
-            conductivity_D = self.conf_data[num + 4],
-            conductivity_PCOR = self.conf_data[num + 5],
-            conductivity_cell_const = self.conf_data[num + 6],
-            conductivity_series_r = self.conf_data[num + 7],
-            conductivity_slope = self.conf_data[num + 8],
-            conductivity_offset = self.conf_data[num + 9],
-            conductivity_GHIJ = self.conf_data[num + 10]
+        split_string_1 = self.conf_data[num + 1].split(' ')
+        split_string_2 = self.conf_data[num + 2].split(' ')
+        return ConductivitySensor(
+            conductivity_sensor_number=self.conf_data[num],
+            conductivity_M=split_string_1[0],
+            conductivity_A=split_string_1[1],
+            conductivity_B=split_string_1[2],
+            conductivity_C=split_string_1[3],
+            conductivity_D=split_string_1[4],
+            conductivity_CPCOR=split_string_1[5],
+            conductivity_cell_const=split_string_2[0],
+            conductivity_series_r=split_string_2[1],
+            conductivity_slope=split_string_2[2],
+            conductivity_offset=split_string_2[3],
+            conductivity_GHIJ=split_string_2[4]
+        )
+
+
+class TemperatureSensorBuilder(SensorBuilder):
+    def get(self, num):
+        split_string_1 = self.conf_data[num + 1].split(' ')
+        return TemperatureSensor(
+            temperature_sensor_number=self.conf_data[num],
+            temperature_F0=split_string_1[0],
+            temperature_A=split_string_1[1],
+            temperature_B=split_string_1[2],
+            temperature_C=split_string_1[3],
+            temperature_D=split_string_1[4],
+            temperature_slope=split_string_1[5],
+            temperature_offset=split_string_1[6],
+            temperature_GHIJ=split_string_1[7]
+        )
+
+
+class PressureSensorBuilder(SensorBuilder):
+    def get(self, num):
+        split_string_1 = self.conf_data[num + 1].split(' ')
+        split_string_2 = self.conf_data[num + 2].split(' ')
+        split_string_3 = self.conf_data[num + 3].split(' ')
+        return PressureSensor(
+            pressure_sensor_number=self.conf_data[num],
+            pressure_T1=split_string_1[0],
+            pressure_T2=split_string_1[1],
+            pressure_T3=split_string_1[2],
+            pressure_T4=split_string_1[3],
+            pressure_T5=split_string_1[4],
+            pressure_C1=split_string_2[0],
+            pressure_C2=split_string_2[1],
+            pressure_C3=split_string_2[2],
+            pressure_C4=split_string_2[3],
+            pressure_D1=split_string_3[0],
+            pressure_D2=split_string_3[1],
+            pressure_slope=split_string_3[2],
+            pressure_offset=split_string_3[3],
+            pressure_sensor_type=split_string_3[4],
+            pressure_AD590_M=split_string_3[5],
+            pressure_AD590_B=split_string_3[6]
         )
 
 
 class FabricSensor:
     fabric = {
         0: ConductivitySensor,
-        1: TemperatureSensor,
-        2: PressureSensor
+        3: TemperatureSensor,
+        10: PressureSensor
     }
 
+    def get(self, num, conf_data):
+        if num in self.fabric:
+            return self.fabric.get(num)(conf_data).get(num)
+        return None
 
-"""
-class ConfigParser:
+
+class LaunchFabric:
     def __init__(self, file_config_path):
-        self.file_config_path: str = file_config_path
-        self.file_data: str = str()
+        file_config_path: str = file_config_path
+        config_file_data: list[str]
 
-        with (open(self.file_config_path, 'r')) as file:
-            self.file_data = file.read()
+        fabric = FabricSensor()
 
-    def config_parse(self) -> ConfData:
-        config_data = ConfData()
+        with (open(file_config_path, 'r')) as file:
+            config_file_data = file.read().splitlines()
+            print(config_file_data)
 
-        regular = r'^\d+$'
-        match = re.search(regular, self.file_data)
-        if match:
-            config_data.conductivity_sensor_number = match[0]
+        for i in range(len(config_file_data)):
+            entity = fabric.get(i, config_file_data)
+            print(entity)
 
-            print(config_data)
-
-        return config_data
-"""
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import config_parser
 from cnv_parser import CnvParser
-from config_parser import ConfigParser
+from config_parser import LaunchFabric
 from export_to_json import ExportToJson
 
 if __name__ == '__main__':
     cnv_data = CnvParser('cnv\\PESCH080.CNV')
-    conf_data = ConfigParser('config\\s80_230321v0134_O2OtObs!.con')
+    conf_data = LaunchFabric('config\\s80_230321v0134_O2OtObs!.con')
 
     cnv_header_data = cnv_data.header_parse()
     cnv_body_data = cnv_data.body_parse()
-    conf_body_data = conf_data.config_parse()
-
-    export_to_json = ExportToJson.export_to_json(conf_body_data, 'D:\\work\\python\\Hydrophysic_works'
-                                                              '\\Parser_measurements_data\\', 'result')
