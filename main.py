3cbe9b48579c0a36007e382b15c5954c6580af86
XuTPuK99 <ivan.tyurnev.99@mail.ru>
2024-07-10 18:08:03 +0800

    init commit

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from cnv_parser import CnvParser
 from conf_parser import ConfParser
 from export_to_json import ExportToJson
-from Tmd_search import TmdSearch
+from tmd_search import TmdSearch
 
 if __name__ == '__main__':
-    cnv_file = CnvParser('cnv\\mLTpup0729.cnv')
+    cnv_file = CnvParser('cnv\\mLT0530.cnv')   #  'cnv\\mLTpup0729.cnv'
     conf_file = ConfParser('config\\s80_230321v0134_O2OtObs!.con')
 
     cnv_header_data = cnv_file.header_parse()
     cnv_body_data = cnv_file.body_parse()
 
     conf_data = conf_file.conf_parse()
+    print(len(cnv_header_data.name_list))
+    print(len(cnv_body_data.table_data[0]))
+    print(cnv_body_data.table_data[1])
 
-    tmd_search = TmdSearch(cnv_header_data, cnv_body_data)
-    tmd_search.search()
+    #tmd_search = TmdSearch(cnv_header_data, cnv_body_data)
+    #tmd_search.search()
 
     #ExportToJson.export_to_json_cnv(cnv_body_data, 'result\\', 'result')
     #ExportToJson.export_to_json_conf(conf_data, 'result\\', 'result')
---
+++ tmd_search.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+class TmdSearch:
+    def __init__(self, cnv_header_data, cnv_body_data):
+        self.pd_cnv_data = pd.DataFrame(columns=cnv_header_data.name_list, data=cnv_body_data.table_data)
+
+    def search(self):
+        k = -0.00000007610308758
+        x = self.pd_cnv_data.iloc[:, 0] ** (2 - 0.0019619296) * self.pd_cnv_data.iloc[:, 0]
+        b = 3.9667
+
+        tdm = (k * x + b)
+
+        second_column = self.pd_cnv_data.columns.size
+        self.pd_cnv_data.insert(loc=second_column, column='Tdm', value=tdm)
+        self.pd_cnv_data.insert(loc=second_column + 1, column='Temperature < Tdm',
+                                value=self.pd_cnv_data.iloc[:, 2] < self.pd_cnv_data.iloc[:, second_column])
+        value_counts = self.pd_cnv_data['Temperature < Tdm'].value_counts()
+
+        print(value_counts)
+        return value_counts
